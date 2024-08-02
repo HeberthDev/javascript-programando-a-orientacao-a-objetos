be4a09c0c2c39bd9aa0c47f1827386582bdeca64
HeberthDev <heberthdeveloper@gmail.com>
2024-08-02 08:34:16 -0300

    Métodos com retorno

--- index.js
+++ index.js
@@ -10,13 +10,16 @@ class ContaCorrente {
     sacar(valor) {
         if (this._saldo >= valor) {
             this._saldo -= valor;
+            return valor;
         }
     }
 
     depositar(valor) {
-        if (valor > 0) {
-            this._saldo += valor;
+        if (valor <= 0) {
+            return;
         }
+
+        this._saldo += valor;
     }
 }
 
@@ -29,16 +32,13 @@ cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
 
 const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.saldo = 0;
 contaCorrenteRicardo.agencia = 1001;
 
-console.log(contaCorrenteRicardo.saldo);
 contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.depositar(200);
-contaCorrenteRicardo.depositar(-1);
-console.log(contaCorrenteRicardo.saldo);
-contaCorrenteRicardo.sacar(50);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
 
-console.log(contaCorrenteRicardo.saldo);
-console.log(cliente1);
-console.log(cliente2);
+const valorSacado = contaCorrenteRicardo.sacar(50);
+console.log(valorSacado);
+
+console.log(contaCorrenteRicardo);
